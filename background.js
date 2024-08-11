68d60c38b9cb368eb81c07b6d6af6f2503fe223e
neropako <github@neropako.im>
2024-08-11 16:11:53 +0200

    fixes

--- background.js
+++ background.js
@@ -1,17 +1,106 @@
-browser.runtime.onInstalled.addListener(() => {
+var bookmarksLocal = [];
 
-    const bookmarkData = JSON.stringify(bookmarks);
-    
-// Save to local storage
-    browser.storage.local.set({ bookmarks: bookmarkData })
-            .then(() => {
-                console.log("Bookmarks saved successfully!");
-            })
-            .catch((error) => {
-                console.error("Error saving bookmarks: ", error);
-            });
-    }).catch((error) => {
-        console.error("Error retrieving bookmarks: ", error);
+function initializeBookmarks() {
+    bookmarksLocal = [];
+    browser.bookmarks.getTree().then((bookmarks) => {
+        bookmarks.forEach((folder) => {
+            if (folder.children) {
+                processBookmarks(bookmarks);
+            }
+        });
+
+        // Convert bookmarks to a format suitable for storage
+        var promises = [];
+
+        bookmarksLocal.forEach(bookmark => {
+            if(!bookmark.favicon) {
+                promises.push(fetch("https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=" + bookmark.url + "&size=50", {
+                    method: "GET",
+                    mode: "cors",
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.blob();
+                })
+                .then(blob => {
+                    const reader = new FileReader();
+                    reader.onloadend = function() {
+                        // The result is a Data URL
+                        const dataUrl = reader.result;
+                        bookmark.favicon = dataUrl;
+                    };
+                    reader.readAsDataURL(blob);
+                })
+                .catch((er) => {
+                    console.log(er);
+                }));
+            }
+        });
+        // Save to local storage
+        Promise.all(promises).then(() => browser.storage.local.set({ "bookmarks": bookmarksLocal }));
     });
+}
 
+  
+function processBookmarks(bookmarks) {
+    bookmarks.forEach((bookmark) => {
+      if (bookmark.children) {
+        // If the bookmark is a folder, recursively process its children
+        processBookmarks(bookmark.children);
+      } else if (bookmark.url) {
+        // If the bookmark is a link, log its title and URL
+        bookmarksLocal.push(bookmark);
+      }
+    });
+  }
+  
+
+browser.runtime.onInstalled.addListener(initializeBookmarks());
+browser.bookmarks.onMoved.addListener(initializeBookmarks());
+browser.bookmarks.onCreated.addListener(initializeBookmarks());
+
+browser.bookmarks.onChanged.addListener((id, changeInfo) => {
+    browser.storage.local.get("bookmarks").then((bookmarks) => {
+        let bookmark = bookmarks.find((el) => el.id == id);
+        var promises = [];
+        if(changeInfo.url) {
+            bookmark.url = changeInfo.url;
+            promises.push(fetch("https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=" + bookmark.url + "&size=50", {
+                method: "GET",
+                mode: "cors",
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                const reader = new FileReader();
+                reader.onloadend = function() {
+                    // The result is a Data URL
+                    const dataUrl = reader.result;
+                    bookmark.favicon = dataUrl;
+                };
+                reader.readAsDataURL(blob);
+            })
+            .catch((er) => {
+                console.log(er);
+            }));
+        }
+        if(changeInfo.title) {
+            bookmark.title = changeInfo.title;
+        }
+        Promise.all(promises).then(() => browser.storage.local.set({ "bookmarks": bookmarks }));
+    });
 });
+
+browser.bookmarks.onRemoved.addListener((id) => {
+    browser.storage.local.get("bookmarks").then((bookmarks) => {
+        bookmarks.splice(bookmarks.indexOf(bookmarks.findIndex(bookmark => bookmark.id == id)), 1);
+        browser.storage.local.set({ "bookmarks": bookmarks });
+    });
+});
+
--- newtab.js
+++ newtab.js
@@ -1,21 +1,15 @@
 const bookmarksList = document.getElementById('favorites-container');
+const searchBox = document.getElementById('search-bar-input');
+var objectUrls = [];
 
-browser.bookmarks.getTree().then((bookmarks) => {
-  bookmarks.forEach((folder) => {
-    if (folder.children) {
-      processBookmarks(bookmarks);
-    }
-  });
+searchBox.addEventListener("change", (event) => {
+  searchBookmarks(event.target.value);
 });
 
-function processBookmarks(bookmarks) {
-  bookmarks.forEach((bookmark) => {
-    if (bookmark.children) {
-      // If the bookmark is a folder, recursively process its children
-      processBookmarks(bookmark.children);
-    } else if (bookmark.url) {
-      // If the bookmark is a link, log its title and URL
-      const bookmarkElement = createBookmarkElement(bookmark);
+function renderBookmarks() {
+  browser.storage.local.get("bookmarks").then((bookmarks) => {
+    for (const item of bookmarks) {
+      const bookmarkElement = createBookmarkElement(item);
       bookmarksList.appendChild(bookmarkElement);
     }
   });
@@ -28,7 +22,9 @@ function createBookmarkElement(bookmark) {
   bookmarkDiv.href=bookmark.url;
   // Create an image element for the favicon
   const faviconImg = document.createElement('img');
-  faviconImg.src = "https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=" + bookmark.url + "&size=50"; // Use a default icon if favicon is not available
+  var imageUrl = URL.createObjectURL(bookmark.favicon);
+  objectUrls.push(imageUrl);
+  document.querySelector("#image").src = imageUrl;
   faviconImg.alt = `${bookmark.title} favicon`;
   bookmarkDiv.appendChild(faviconImg);
 
@@ -43,14 +39,45 @@ function createBookmarkElement(bookmark) {
   return bookmarkDiv;
 }
 
-function searchBookmarks(query) {
-  bookmarksList.replaceChildren();
-  browser.bookmarks.search(query).then((bookmarkItems) => {
-    for (const item of bookmarkItems) {
-      const bookmarkElement = createBookmarkElement(item);
-      bookmarksList.appendChild(bookmarkElement);
-    }
-  });
+function createClearSearchElement() {
+  // Create a div element to hold the bookmark information
+  const bookmarkDiv = document.createElement('a');
+  bookmarkDiv.className = 'favorites-item';
+  bookmarkDiv.onclick="searchBox.value = ''; searchBookmarks(''); return false;";
+  // Create an image element for the favicon
+  const faviconImg = document.createElement('img');
+  faviconImg.src = "images/icons8-clear-50.png"; // Use a default icon if favicon is not available
+  faviconImg.alt = `Clear search favicon`;
+  bookmarkDiv.appendChild(faviconImg);
+
+  // Create a span element for the bookmark title
+  const titleSpan = document.createElement('span');
+  titleSpan.textContent = "Clear search";
+
+  // Append the favicon and title to the bookmark div
+
+  bookmarkDiv.appendChild(titleSpan);
+
+  return bookmarkDiv;
 }
 
+function searchBookmarks(query) {
+  objectUrls.forEach((el) => URL.revokeObjectURL(el));
+  objectUrls = [];
+  bookmarksList.replaceChildren();
+  if(query && query.length > 0) {
+    bookmarksList.appendChild(createClearSearchElement());
+    browser.storage.local.get("bookmarks").then((bookmarks) => {
+      let bookmarkItems = bookmarks.filter((bookmark) => bookmark.name.toLowerCase().includes(query.toLowerCase()) || bookmark.url.toLowerCase().includes(query.toLowerCase()));
+      for (const item of bookmarkItems) {
+        const bookmarkElement = createBookmarkElement(item);
+        bookmarksList.appendChild(bookmarkElement);
+      }
+    });
+  } else {
+    renderBookmarks();
+  }
+}
+
+renderBookmarks();
 
